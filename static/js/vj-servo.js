7a70b8d7df39809fc514cfd0ce6bb95e4072b155
Juri Berlanda <juriberlanda@hotmail.com>
2016-04-14 21:42:02 +0200

    [Web] Fix Javascript indentation.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- static/js/vj-servo.js
+++ static/js/vj-servo.js
@@ -1,15 +1,15 @@
-    console.log("Start");
-    var socket = io.connect('http://' + document.domain + ':' + location.port + '/servo');
+console.log("Start");
+var socket = io.connect('http://' + document.domain + ':' + location.port + '/servo');
 
-    socket.on('connect', function(msg) {
-        console.log('[INFO] Socket connected.');
-    });
+socket.on('connect', function(msg) {
+    console.log('[INFO] Socket connected.');
+});
 
-    function sendPosition() {
-        console.log('sendPosition');
-        console.log('value: ' + $('#position').val());
-        socket.emit('moveTo', $('#position').val());
-    }
+function sendPosition() {
+    console.log('sendPosition, value: ' + $('#position').val());
+    socket.emit('moveTo', $('#position').val());
+}
+
 function sendStop() {
     console.log('sendStop');
     socket.emit('stop');
